4da4568b322c7b59b13f01d2e74354f12e72dc6e
Écran Forteça <ekranetilmeyletre2004@gmail.com>
2024-04-16 18:41:51 -0400

    Agregada página de 'ventas'.

---
+++ js/test/prueba-tabla-ventas.js
@@ -0,0 +1,18 @@
+let i = 0;
+while (i < 100)
+{
+	document.write
+	(`
+		<tr>
+			<td>datos</td>
+			<td>datos</td>
+			<td>{entrada manual}</td>
+			<td>datos</td>
+			<td>
+				<a style='text-decoration: none; font-weight: bold;' href='#'>Eliminar</a>
+			</td>
+		</tr>
+	`);
+
+	i++;
+}
