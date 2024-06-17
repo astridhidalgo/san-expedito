bad22d5d9d47dbc62030bb9d4c47ae78dda8bfaf
Écran Forteça <ekranetilmeyletre2004@gmail.com>
2024-06-17 10:42:56 -0400

    Página de administración de usuarios agregada.

--- js/botones-rol.js
+++ js/botones-rol.js
@@ -2,16 +2,20 @@ let rolActual = sessionStorage.getItem("rol");
 let botonProductos = document.querySelector(".BotonMenu.Productos");
 let botonCategorias = document.querySelector(".BotonMenu.Categorias");
 let botonProveedores = document.querySelector(".BotonMenu.Proveedores");
+let botonUsuarios = document.querySelector(".BotonMenu.Usuarios");
 
 if (rolActual == "admin")
 {
 	botonProductos.style = "display: block;";
 	botonCategorias.style = "display: block;";
 	botonProveedores.style = "display: block;";
+	botonProveedores.style = "display: block;";
+	botonUsuarios.style = "display: block;";
 }
 	else
 	{
 		botonProductos.style = "display: none;";
 		botonCategorias.style = "display: none;";
 		botonProveedores.style = "display: none;";
+		botonUsuarios.style = "display: none;";
 	}
---
+++ js/lista-usuarios.js
@@ -0,0 +1,17 @@
+for(let i = 0; i < 20; i++)
+{
+	document.write
+	(`
+		<tr>
+			<td>datos</td>
+			<td>datos</td>
+			<td>datos</td>
+			<td>datos</td>
+			<td>datos</td>
+			<td>datos</td>
+			<td>
+				<button class="eliminarBtn" type="button">Eliminar</button>
+			</td>
+		</tr>
+	`);
+}
